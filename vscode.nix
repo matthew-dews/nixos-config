d9dabfe4a0a035b259ffd68279bcff8a1b56b310
Corinne Corinfinite <corinfinite@gmail.com>
2024-09-08 19:42:20 -0400

    update vscode setup

--- vscode.nix
+++ vscode.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   programs.vscode = {
@@ -6,8 +6,25 @@
     package = pkgs.vscode;
     extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
+      jkillian.custom-local-formatters
       ms-python.python
     ];
-    userSettings = { };
+    userSettings = builtins.fromJSON ''
+      {
+          "[nix]": {
+              "editor.defaultFormatter": "jkillian.custom-local-formatters"
+          },
+          "customLocalFormatters.formatters": [
+              {
+                  "command": "nixfmt",
+                  "languages": [
+                      "nix"
+                  ]
+              }
+          ],
+          "git.autofetch": true,
+          "git.suggestSmartCommit": false
+      }
+    '';
   };
 }
